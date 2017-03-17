708d5564023e278c86fb4686a8f83e0714ac5625
Lars Tingelstad <lars.tingelstad@ntnu.no>
2017-03-17 11:30:13 +0100

    travis config

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ class CMakeBuild(build_ext):
             # build_args += ['--', '/m'# ]
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            cmake_args += ['-DCMAKE_CXX_COMPILER=g++-5']
-            cmake_args += ['-DCMAKE_C_COMPILER=gcc-5']
+            # cmake_args += ['-DCMAKE_CXX_COMPILER=g++-5']
+            # cmake_args += ['-DCMAKE_C_COMPILER=gcc-5']
             build_args += ['--', '-j2']
 
         env = os.environ.copy()
